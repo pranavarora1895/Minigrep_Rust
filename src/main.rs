299d89171449ac3db7e9a4a7ec74b7934eb201c4
Pranav Arora <aurorapranav187@gmail.com>
2022-02-17 09:57:54 +0530

    Removed Dead code

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use minigrep::Config;
 use std::env;
 use std::process;
 fn main() {
-    let args: Vec<String> = env::args().collect();
     let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
