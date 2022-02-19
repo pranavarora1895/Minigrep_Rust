247d9d9133248ed3a772b82dad208da708749453
Pranav Arora <aurorapranav187@gmail.com>
2022-02-19 21:44:28 +0530

    Publishing to crates.io

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
+//! # MiniGrep ![Rust](https://img.shields.io/badge/rust-%23000000.svg?style=for-the-badge&logo=rust&logoColor=white)
+//!
+//! ### This is a Command line utility developed in Rust that parses the keyword from the file and outputs the relevant line.
+//!
+//! ### Clone this repo and follow the given usage:
+//!
+//! > ## Usage
+//! >
+//! > * `cargo run <keyword> <filename.txt>`
+//! > * Setup the `CASE_INSENSITIVE` environment variable
+//! > * On your PowerShell run the following commands:
+//! >```
+//! > $Env CASE_INSENSITIVE=1
+//! > cargo run <keyword> <filename.txt>
+//! >```
+//! > * To deactivate the Environment Variable, run the following command:
+//! >  
+//! > `Remove-Item Env:CASE_INSENSITIVE`
+//! ---
+//!
+//! * Follow Me On Instagram at [Pranav Arora](https://www.instagram.com/arorapranav187)
+//! * Lets Get Connected on Linkedin at [Pranav Arora](https://www.linkedin.com/in/pranav-arora-354b71bb/)
+//!
+//! ### ThankYou!
+
 use std::env;
 use std::error::Error;
 use std::fs;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use minigrep::Config;
 use std::env;
 use std::process;
+
 fn main() {
     let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
